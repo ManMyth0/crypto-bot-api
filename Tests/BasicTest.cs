3a0c0bdd7631ea124684ddc79d0add6ed4b9c8a8
Your Name <adplatonic@gmail.com>
2025-05-12 00:33:11 -0400

    -Setup MSTest framework and Moq

---
+++ Tests/BasicTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace crypto_bot_api.Tests
+{
+    [TestClass]
+    public class BasicTest
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            // A basic test that should always pass
+            Assert.IsTrue(true);
+        }
+    }
+} 
